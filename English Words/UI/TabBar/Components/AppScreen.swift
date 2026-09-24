880d46fa2107bfba0daaa2e57a233758a42bb7f1
DERAPTER <egorxalikov@gmail.com>
2026-09-25 00:21:46 +0300

    Created AppScreen enum

--- English Words/UI/TabBar/Components/AppScreen.swift
+++ English Words/UI/TabBar/Components/AppScreen.swift
@@ -5,3 +5,30 @@
 //  Created by Егор Халиков on 23.09.2026.
 //
 
+import Foundation
+
+enum AppScreen: CaseIterable {
+    case cardsGroups
+    case solveCards
+    case profile
+    
+    var titleKey: String {
+        switch self {
+        case .cardsGroups: return "tab_cards"
+        case .solveCards:  return "tab_solve"
+        case .profile:     return "tab_profile"
+        }
+    }
+    
+    var iconName: String {
+        switch self {
+        case .cardsGroups: return "square.stack.3d.up.fill"
+        case .solveCards:  return "play.circle.fill"
+        case .profile:     return "person.crop.circle.fill"
+        }
+    }
+    
+    var title: String {
+        titleKey.localized()
+    }
+}
