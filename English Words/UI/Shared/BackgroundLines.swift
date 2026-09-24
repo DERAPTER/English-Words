7f64915f5eeb76ae18c384eb8a56c2dc69ac24f8
DERAPTER <egorxalikov@gmail.com>
2026-09-25 01:37:25 +0300

    Created BackgroundLines

--- English Words/UI/Shared/BackgroundLines.swift
+++ English Words/UI/Shared/BackgroundLines.swift
@@ -5,3 +5,23 @@
 //  Created by Егор Халиков on 23.09.2026.
 //
 
+import SwiftUI
+
+struct BackgroundLines: View {
+    var body: some View {
+        ZStack {
+            Path { path in
+                path.move(to: CGPoint(x: 0, y: 100))
+                path.addLine(to: CGPoint(x: 200, y: 300))
+            }
+            .stroke(Color.brown.opacity(0.08), lineWidth: 2)
+            
+            Circle()
+                .fill(Color.brown.opacity(0.03))
+                .frame(width: 300, height: 300)
+                .offset(x: 200, y: -50)
+        }
+        .ignoresSafeArea()
+        .allowsHitTesting(false)
+    }
+}
