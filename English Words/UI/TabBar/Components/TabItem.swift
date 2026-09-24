6128e74dd8314a5bfdc1cce96128c3310b5e8298
DERAPTER <egorxalikov@gmail.com>
2026-09-25 00:23:23 +0300

    Created TabItem

--- English Words/UI/TabBar/Components/TabItem.swift
+++ English Words/UI/TabBar/Components/TabItem.swift
@@ -5,3 +5,16 @@
 //  Created by Егор Халиков on 23.09.2026.
 //
 
+import Foundation
+
+struct TabItem: Identifiable, Hashable {
+    let screen: AppScreen
+    
+    var id: AppScreen { screen }
+    var title: String { screen.title }
+    var iconName: String { screen.iconName }
+    
+    static var all: [TabItem] {
+        AppScreen.allCases.map(TabItem.init(screen:))
+    }
+}
