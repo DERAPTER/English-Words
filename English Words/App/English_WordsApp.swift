2fda9017e84402f4afd3aaf359375af25525f3e4
DERAPTER <egorxalikov@gmail.com>
2026-09-25 01:45:17 +0300

    Created @main struct

--- English Words/App/English_WordsApp.swift
+++ English Words/App/English_WordsApp.swift
@@ -5,3 +5,43 @@
 //  Created by Егор Халиков on 23.09.2026.
 //
 
+import SwiftUI
+import SwiftData
+
+@main
+struct English_WordsApp: App {
+    
+    private let modelContainer: ModelContainer
+    private let appContainer: AppContainer
+    
+    init() {
+        // 1. Собираем ModelContainer
+        let container: ModelContainer
+        do {
+            container = try ModelContainer(
+                for: Card.self,
+                CardGroup.self,
+                DailyStat.self,
+                UserSettings.self
+            )
+        } catch {
+            fatalError("Не удалось создать ModelContainer: \(error)")
+        }
+        self.modelContainer = container
+        
+        // 2. Миграция из старого JSON (если нужно)
+        let migrator = JSONToSwiftDataMigrator(context: container.mainContext)
+        migrator.migrateIfNeeded()
+        
+        // 3. Собираем DI-контейнер
+        self.appContainer = AppContainer.live(container: container)
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+                .environment(appContainer)
+                .modelContainer(modelContainer)
+        }
+    }
+}
