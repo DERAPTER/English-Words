1d809f9535ef1216f130aee3e79ef41333b9ecb4
DERAPTER <egorxalikov@gmail.com>
2026-09-24 21:40:00 +0300

    Created AppLanguage enum

--- English Words/UI/Localization/AppLanguage.swift
+++ English Words/UI/Localization/AppLanguage.swift
@@ -5,3 +5,27 @@
 //  Created by Егор Халиков on 23.09.2026.
 //
 
+import Foundation
+
+enum AppLanguage: String, CaseIterable, Codable {
+    case english = "en"
+    case russian = "ru"
+    
+    var displayName: String {
+        switch self {
+        case .english: return "English"
+        case .russian: return "Русский"
+        }
+    }
+    
+    var flagEmoji: String {
+        switch self {
+        case .english: return "🇬🇧"
+        case .russian: return "🇷🇺"
+        }
+    }
+    
+    var locale: Locale {
+        Locale(identifier: rawValue)
+    }
+}
